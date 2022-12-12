999bee6a55af895b233c2c3179e9e893e0f8a519
matthieu Duprat <matthieu.duprat@angano.fr>
2022-12-13 00:11:01 +0100

    next

--- static/js/index.js
+++ static/js/index.js
@@ -5,31 +5,25 @@ window.addEventListener('DOMContentLoaded',function(){
 
     // gestion affichage tableau de données
     $('.md-btn-display').on('click',function(e){
-       $('#'+e.target.dataset['target']).toggle()
+       $('#'+e.target.dataset['target']).toggle().css('z-index',10).css('position','relative').css('background-color','white')
     })
 
-    $('span[data-span]').on('click',function(e){
+    // gestion affichage des programmations
+    $('div[data-span]').on('click',function(e){
         let ul = '<ul><li class="btn btn-sm btn-success">+ Programmation</li>'
         let xhr = new XMLHttpRequest();
         xhr.open('get',url+'/api/programmation/'+e.target.dataset['appareil'])
         xhr.responseType ='json'
         xhr.onload = function(){
-            console.log(this.response)
+     
            for(let toto in this.response){
-            //Vendredi: 00:00=>17:55 Del Edit
+            
             ul = ul + `<li class="d-flex justify-content-between mx-2"><div>${days[(this.response[toto].day)-1]}: ${this.response[toto].heure_debut}:${this.response[toto].min_debut}=>${this.response[toto].heure_fin}:${this.response[toto].min_fin}</div><div><span class="btn btn-sm btn-danger">X</span></div> </li>`
         }
         //console.log(e.target.dataset['span'])
         ul = ul + '</ul>'
-        $('#'+e.target.dataset['span']).html(ul).toggle()
-       
-   
-    }
-    $('body').on('cick',function(){
-
-    })
-           
-        
+        $('#'+e.target.dataset['span']).html(ul).toggle()   
+        }
         xhr.send()
     })
 
@@ -44,7 +38,7 @@ window.addEventListener('DOMContentLoaded',function(){
             if(xhr.status===200 && xhr.readyState===4){
                 e.target.className = 'btn btn-sm btn-primary'
                 $('#md-calendar-'+e.target.dataset.appareil).text('Changement pris en compte').show()
-                .css('padding','1rem').css('width','max-content').css('border','solid 1px #dbcece').css('margin-left','5px')
+                .css('padding','1rem').css('width','max-content').css('border','solid 1px #dbcece').css('margin-left','5px').css('z-index',10)
                 .addClass('bg-light text-dark')
                 setTimeout(()=>{
                     $('#md-calendar-'+e.target.dataset.appareil).text('').hide();
